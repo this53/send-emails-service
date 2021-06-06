737a25bbc044af853a15588e41b4e084e093f422
Douglas Bernardes <douglaspb96@gmail.com>
2021-06-06 00:44:33 -0300

    Add

--- src/modules/send-emails/jobs/send-custom-email/send-custom-email.queue.ts
+++ src/modules/send-emails/jobs/send-custom-email/send-custom-email.queue.ts
@@ -5,7 +5,7 @@ export const SendCustomEmailQueue = BullModule.registerQueue({
     name: SEND_CUSTOM_EMAIL_QUEUE,
     limiter: {
         duration: 1000,
-        max: 1,
+        max: 1000,
     },
     defaultJobOptions: {
         attempts: 3,
