5152f699cb7a3c489c6b1b944ce8d2ce74acba21
Stokei Oficial <stokeioficial@gmail.com>
2021-08-09 16:40:32 -0300

    Add new redis config

--- src/config/bull/index.ts
+++ src/config/bull/index.ts
@@ -3,6 +3,7 @@ import { redisConfig } from '../redis';
 
 export const bullConfig: BullModuleOptions = {
     redis: {
+        username: redisConfig.username,
         host: redisConfig.host,
         password: redisConfig.password,
         port: redisConfig.port,
