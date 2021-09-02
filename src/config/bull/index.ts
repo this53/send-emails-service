9d40b45e75bdffefc48c2baa90493bee13a9bd4f
Stokei Oficial <stokeioficial@gmail.com>
2021-09-02 06:26:58 -0300

    Add redis tls production

--- src/config/bull/index.ts
+++ src/config/bull/index.ts
@@ -1,12 +1,14 @@
 import { BullModuleOptions } from '@nestjs/bull';
+import { isProduction } from '~/environments';
 import { redisConfig } from '../redis';
 
 export const bullConfig: BullModuleOptions = {
-    redis: {
-        username: redisConfig.username,
-        host: redisConfig.host,
-        password: redisConfig.password,
-        port: redisConfig.port,
-        connectTimeout: redisConfig.connectTimeout,
-    },
+  redis: {
+    username: redisConfig.username,
+    host: redisConfig.host,
+    password: redisConfig.password,
+    port: redisConfig.port,
+    connectTimeout: redisConfig.connectTimeout,
+    ...(isProduction && { tls: {} })
+  }
 };
